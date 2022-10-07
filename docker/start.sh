f306df4e26ef80a3d53a34268f27ac2182b6572c
zhujinhua <zhujinhua@cestc.cn>
2022-10-07 15:36:18 +0800

    需按照requirements依赖

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /root/cityie_nlp/pipelines/
+cd /home/cityie_nlp/pipelines/
 #sh create_index.sh
 nohup sh run_server.sh > server.log 2>&1 &
 nohup sh run_client.sh > client.log 2>&1 &
